77ea3b8600db72647fa08f3aeca92f6af783a3ac
Kevin Jaramillo C <kevinjaramillocorrea@gmail.com>
2018-05-05 00:17:46 -0500

    Add the sprites

---
+++ Final Project/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour 
+{
+	[Header("Buttons")]
+	public GameObject buttonPlay;
+	public GameObject buttonHelp;
+	public GameObject pauseButton;
+	[Header("Panel and Tittle")]
+	public GameObject panel;
+	public GameObject tittle;
+	
+	private void Update () 
+	{
+		
+	}
+
+	public void PlayMode()
+	{
+		
+		panel.SetActive(false);
+		tittle.SetActive(false);
+	}
+}
