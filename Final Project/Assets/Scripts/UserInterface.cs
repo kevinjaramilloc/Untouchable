de21d6578e2c7f6affcdafad73a2d57e7d8557f1
Kevin Jaramillo C <kevinjaramillocorrea@gmail.com>
2018-05-08 15:35:37 -0500

    Add the occlusion culling at the game

--- Final Project/Assets/Scripts/UserInterface.cs
+++ Final Project/Assets/Scripts/UserInterface.cs
@@ -5,22 +5,61 @@ using UnityEngine;
 public class UserInterface : MonoBehaviour 
 {
 	[Header("Buttons")]
-	public GameObject buttonPlay;
-	public GameObject buttonHelp;
-	public GameObject pauseButton;
+	public GameObject playButton;
+	public GameObject helpButton;
+	public GameObject backButton;
+	public GameObject creditsButton;
 	[Header("Panel and Tittle")]
 	public GameObject panel;
 	public GameObject tittle;
+	public GameObject highConcept;
+	[Header("Information")]
+	public GameObject credits;
+	public GameObject instructions;
 	
 	private void Update () 
 	{
 		
 	}
 
+	public void MainUI()
+	{
+		tittle.SetActive(true);
+		highConcept.SetActive(true);
+		helpButton.SetActive(true);
+		playButton.SetActive(true);
+		creditsButton.SetActive(true);
+	}
+
 	public void PlayMode()
 	{
-		
 		panel.SetActive(false);
 		tittle.SetActive(false);
+		highConcept.SetActive(false);
+		helpButton.SetActive(false);
+		playButton.SetActive(false);
+		creditsButton.SetActive(false);
+	}
+
+	public void Instructions()
+	{
+		instructions.SetActive(true);
+		backButton.SetActive(true);
+		tittle.SetActive(false);
+		highConcept.SetActive(false);
+		helpButton.SetActive(false);
+		playButton.SetActive(false);
+		creditsButton.SetActive(false);
+	}
+
+	public void Credits()
+	{
+		credits.SetActive(true);
+		backButton.SetActive(true);
+		tittle.SetActive(false);
+		highConcept.SetActive(false);
+		helpButton.SetActive(false);
+		playButton.SetActive(false);
+		creditsButton.SetActive(false);
 	}
 }
